0b1ddc66513bd6ae163c72b2a2d24c48baebd02e
ZeroWraith <krapyakid@gmail.com>
2025-06-22 09:43:19 +0700

    main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
- document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function() {
     // Kebab Menu Logic
     const menuToggle = document.getElementById('menu-toggle');
     const kebabMenu = document.getElementById('kebab-menu');
@@ -12,6 +12,13 @@
         // Close the menu if clicked outside
         window.addEventListener('click', function(e) {
             if (kebabMenu.classList.contains('show') && !kebabMenu.contains(e.target) && e.target !== menuToggle) {
+                kebabMenu.classList.remove('show');
+            }
+        });
+
+        // Close menu when a link is clicked
+        kebabMenu.addEventListener('click', function(e) {
+            if (e.target.tagName === 'A') {
                 kebabMenu.classList.remove('show');
             }
         });
